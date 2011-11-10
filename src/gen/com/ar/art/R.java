ad88939b5bd64ca9b58cdada29ac6af98e618a5b
luli <lulirez@gmail.com>
2011-11-10 16:35:45 -0300

    asdfasdfa

--- src/gen/com/ar/art/R.java
+++ src/gen/com/ar/art/R.java
@@ -17,10 +17,11 @@ public final class R {
     public static final class drawable {
         public static final int background=0x7f020000;
         public static final int ic_launcher=0x7f020001;
-        public static final int icon=0x7f020002;
-        public static final int logo=0x7f020003;
-        public static final int lupa=0x7f020004;
-        public static final int main_button=0x7f020005;
+        public static final int ic_menu_search=0x7f020002;
+        public static final int icon=0x7f020003;
+        public static final int logo=0x7f020004;
+        public static final int lupa=0x7f020005;
+        public static final int main_button=0x7f020006;
     }
     public static final class id {
         public static final int View01=0x7f07000e;
