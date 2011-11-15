150802ea1d46a66c32d4c2bb7ff89372ff37e8cf
Alan Pierri <alanpierri@gmail.com>
2011-11-15 18:55:12 -0300

    axzc

---
+++ src/src/errors/AlertError.java
@@ -0,0 +1,41 @@
+package errors;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
+public class AlertError {
+
+	public AlertError(Activity a, String s) {
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(a);
+
+		builder.setMessage(s).setCancelable(
+
+		false).setPositiveButton("Yes",
+
+		new DialogInterface.OnClickListener() {
+
+			public void onClick(DialogInterface dialog, int id) {
+
+			}
+
+		}).setNegativeButton("No",
+
+		new DialogInterface.OnClickListener() {
+
+			public void onClick(DialogInterface dialog, int id) {
+
+				dialog.cancel();
+
+			}
+
+		});
+
+		AlertDialog alert = builder.create();
+
+		alert.show();
+
+	}
+
+}
